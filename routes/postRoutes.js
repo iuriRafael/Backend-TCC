a6afdf25cc5d1eff22da68ee34092e3051804028
Iuri <iuri-rdutra2@educar.rs.gov.br>
2023-09-26 14:12:17 -0300

    arrumação

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/postControllers');
+const Post = require('../model/post');
 
   
   // Rota para upload de imagem com descrição e localização
   router.post('/upload', imageController.uploadImage);
   // Rota para listar as imagens
   router.get('/list', imageController.listPosts);
+
+  router.get('/user/:userId', async (req, res) => {
+    try {
+      const userId = req.params.userId;
+
+      const userPublications = await Post.find({ userId: userId });
+  
+      res.status(200).json(userPublications);
+    } catch (error) {
+      console.error('Erro ao buscar as publicações do usuário:', error);
+      res.status(500).json({ error: 'Erro ao buscar as publicações do usuário' });
+    }
+  });
   
 module.exports = router;
